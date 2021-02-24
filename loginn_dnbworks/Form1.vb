b27ec24b5cfddad442bc683da82e82d6d1a8f888
dnbworks <Dooyongnsaakobalderama@gmail.com>
2021-02-24 18:42:00 +0800

    vb login

---
+++ loginn_dnbworks/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        imports System.data.OleDb
+        Imports System. Data
+        Public Classs form1
+        Dim provider As String
+        Dim connString As String
+        Dim myConn As oleDbConnection = New OleDbConnection
+
+        Public dr As OleDbDataReader
+        Dim Attempt As Integer = 1
+    Private Sub Button1_click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        myConn.Open()
+        Dim cmd As OleDbCommand = New OleDbCommand("Select * From [LOG] where [USERNAME] = '" & TextBox1 & " ' and [PASSWORD] = '" & TextBox2.Text & "'", myConn)
+        Dim dr As oledbDatareader = ProcessCmdKey.ExecuteReader
+
+        Dim userfound As Boolean = False
+        Dim Name As String = ""
+        Dim gender As String = ""
+
+        While Drawing.Read
+            userfound = True
+            Name = Drawing("Username").toString
+            AutoScrollPosition = Drawing("Position").toStriung
+
+
+
+        End While
+        If userfound = True Then
+            Form2.show()
+            Me.hide()
+            Form2.label4.text = "" & Name & ""
+            Form1.label5.text = "" & AutoScrollPosition & ""
+        ElseIf attempt = 3 Then
+            msgbox("MAXIMUM NUMBER OF Attemps(3), program will now close", MessageBox1con.stop)
+            Me.hide()
+        Else
+            MsgBox("Please, Input the correct Username/Password,Please re-enter,you currently hav reached attempt " & atempt & " of 3.", MessageBoxIcon.Exclamation)
+            Attempt = Attempt + 1
+
+
+        End If
+        My(conn.close())
+
+
+                                []
+
+    End Sub
+    Private Sub form1_load(ByVal Sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+eProvider = "Provider= Microsoft.Ace.OLEDB.12.0;DataSource="|DataDirectory|\LOG.accdb
+        connString = provider
+        myConn.ConnectionString = ConnString
+        TextBox2.UseSystemPasswordChar = True
+    End Sub
+End Class
---
+++ loginn_dnbworks/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("loginn_dooyong")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("loginn_dooyong")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ffc6dd50-8a5d-4d91-8f4a-46736042145e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
